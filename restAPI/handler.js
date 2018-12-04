dd7d55793f9bc763222f79acfe6260599e86ea4e
zubin | zeuscoder <zubin.pratap@gmail.com>
2018-12-04 15:14:02 +1100

    Step 3B: require deps in the handler.js file. create db instance

--- restAPI/handler.js
+++ restAPI/handler.js
@@ -1,12 +1,17 @@
-'use strict';
+"use strict";
+
+const uuid = require("uuid");
+const AWS = require("aws-sdk");
+
+const dynamoDb = AWS.DynamoDB.DocumentClient({});
 
 module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      message: "Go Serverless v1.0! Your function executed successfully!",
+      input: event
+    })
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
