52b59e95c72fc54ea664007cfefe583335628803
zubin | zeuscoder <zubin.pratap@gmail.com>
2018-12-04 11:12:32 +1100

    change handlers hello function. deploy only function with sls deploy function --function hello.  updated response!

--- sampleService/handler.js
+++ sampleService/handler.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
 module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      message: "CHANGED THIS PART! Your function executed successfully!",
+      input: event
+    })
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
