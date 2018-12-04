b169fc596a15a4ef30f3ecbf4fb53475f145ba68
zubin | zeuscoder <zubin.pratap@gmail.com>
2018-12-04 13:10:45 +1100

    refactor Handler to get rid of callbacks which are probs for lambda apparently. WORKING

--- sampleService/APIs/axios.js
+++ sampleService/APIs/axios.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
-// const util = require("util");
-const URL = "http://jsonplaceholder.typicode.com/users/";
+
+const URL = "http://jsonplaceholder.typicode.com/users";
 
 const fetchUsers = async () => {
   console.log("<< helper axios function has been hit >>");
--- sampleService/handler.js
+++ sampleService/handler.js
@@ -1,20 +1,13 @@
 "use strict";
 
-// const axios = require("axios");
 const { fetchUsers } = require("./APIs/axios");
 
-// const URL = "http://jsonplaceholder.typicode.com/users";
-
 module.exports.hello = async (event, context, callback) => {
-  // const res = await axios.get(URL);
   const res = await fetchUsers();
-
-  const response = {
+  return {
     statusCode: 200,
     body: JSON.stringify(res.data)
   };
-
-  callback(null, response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
